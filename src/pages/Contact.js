641446f774f914bfea207a9405aa93826ef37974
Denzerson1 <130482776+Denzerson1@users.noreply.github.com>
2025-06-23 14:09:48 +0200

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,10 +14,10 @@ export default function ContactPage() {
     e.preventDefault();
 
     emailjs.send(
-      'service_7iwx57e',
-      'template_rxfo5ql',
+      'service_b821pby',
+      'template_e1ybklb',
       formData,
-      'wsM6-xa5o51TKoqM_'
+      'KCKyiWCGEFuiFKWNO'
     )
     .then(() => {
       setSuccess(true);
